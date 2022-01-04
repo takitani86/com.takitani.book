5104b55919d946334e8b86c1634fa7ba45084846
takitani <aetheru@gmail.com>
2022-01-04 19:32:37 +0900

    220104 P-151 게시글 조회기능까지

---
+++ src/main/java/com/takitani/book/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,23 @@
+package com.takitani.book.springboot.web.dto;
+
+import com.takitani.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+
+    private Long id;
+    private String title;
+    private String author;
+    private String content;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
