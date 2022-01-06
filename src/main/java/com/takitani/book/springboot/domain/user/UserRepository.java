5639e2de3fc5fcee1d94b7a33497f19c0bbe154a
takitani <aetheru@gmail.com>
2022-01-07 00:37:24 +0900

    220106 P-188 스프링 시큐리티 설정

--- src/main/java/com/takitani/book/springboot/domain/user/UserRepository.java
+++ src/main/java/com/takitani/book/springboot/domain/user/UserRepository.java
@@ -1,8 +1,10 @@
 package com.takitani.book.springboot.domain.user;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 
-public interface UserRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String email);
 }
